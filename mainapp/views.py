4bb6a3bb1dce79d80ce9d4e779d030401b969678
willdrevno <wdrevno@gmail.com>
2013-04-25 00:11:23 -0700

    fixing csrf

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,13 +2,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, render
 from django.template import RequestContext, Context
 from mainapp.models import *
+import datetime
 
 
 def submit(request):
     if request.method == 'POST':
         idea_title_form = request.POST['idea_title']
         idea_text_form = request.POST['idea_text']
-        #idea_created_form = datetime.datetime.now()
+        idea_created_form = datetime.datetime.now()
         idea_last_activity_form = datetime.datetime.now()
         idea = Idea(idea_title=idea_title_form, idea_text=idea_text_form, idea_created=idea_created_form, idea_last_activity=idea_last_activity_form)
         idea.save()

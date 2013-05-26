e1338aa023eb94d7c56fd1470d869217b9556221
willdrevno <wdrevno@gmail.com>
2013-05-26 14:07:01 -0700

    fixing form fields

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class IdeaForm(forms.Form):
-    idea_title = forms.CharField(required=True, label='', initial='Enter idea title...')
-    idea_text = forms.CharField(required=True, widget=forms.Textarea, label='', initial='Describe your idea...')
+    idea_title = forms.CharField(required=True, label='', initial='Enter idea title...', max_length=100)
+    idea_text = forms.CharField(required=True, widget=forms.Textarea, label='', initial='Describe your idea...', max_length=1000)
     
 class CommentForm(forms.Form):
     comment_text = forms.CharField(required=True, widget=forms.Textarea, label='', max_length=1000)

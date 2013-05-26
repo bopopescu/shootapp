50e7f5cc586edf26054a33308fcffe4867b73501
willdrevno <wdrevno@gmail.com>
2013-05-26 00:14:33 -0700

    adding bootstrap

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class IdeaForm(forms.Form):
-    idea_title = forms.CharField(required=True, label='Idea')
-    idea_text = forms.CharField(required=True, widget=forms.Textarea, label='Description')
+    idea_title = forms.CharField(required=True, label='', initial='Enter idea title...')
+    idea_text = forms.CharField(required=True, widget=forms.Textarea, label='', initial='Describe your idea...')
     
 class CommentForm(forms.Form):
     comment_text = forms.CharField(required=True, widget=forms.Textarea, label='')
